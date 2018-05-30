ceb56e3e7a5e4c586fbff1b62292295b80c4d08d
atsuo1203 <dondonkitemasu@gmail.com>
2018-05-30 23:03:49 +0900

    一つのセル完成

--- src/components/Top/container/contentsParts/ThreadContents.js
+++ src/components/Top/container/contentsParts/ThreadContents.js
@@ -1,27 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-
 import {FlatButton} from 'material-ui';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import {IdStyle, TitleStyle, SpeedStyle, CountStyle, ChildrenStyle} from './style/ThreadContentsStyle'
 
 class ThreadContents extends Component {
   static propTypes = {
   }
 
   render() {
-    const IdStyle = styled.div`
-    `
-    const TitleStyle = styled.div`
-    `
-    const SpeedStyle = styled.div`
-    `
-    const CountStyle = styled.div`
-    `
-    const ChildrenStyle = styled.div`
-      display: flex;
-      flex-direction: row;
-    `
     const idObj = (<IdStyle>{1}</IdStyle>)
     const titleObj = (<TitleStyle>{'title'}</TitleStyle>)
     const speedObj = (<SpeedStyle>{'勢い: 3000'}</SpeedStyle>)
@@ -30,14 +19,15 @@ class ThreadContents extends Component {
       <MuiThemeProvider >
         <FlatButton
           style={{position: 'relative', width: '100%'}}
-          children={<ChildrenStyle key={1}>
+          children={
+          <ChildrenStyle key={1}>
             {idObj}
             {titleObj}
             {speedObj}
             {countObj}
           </ChildrenStyle>}
           backgroundColor='#7ca6ff'
-          hoverColor='#7cb6ff'
+          hoverColor='#7cc6ff'
         />
       </MuiThemeProvider>
     );
---
+++ src/components/Top/container/contentsParts/style/ThreadContentsStyle.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const IdStyle = styled.div`
+  border-right: solid 1px #000000;
+  position: relative;
+  width: 5vw;
+`
+export const TitleStyle = styled.div`
+  border-right: solid 1px #000000;
+  position: relative;
+  width: 65vw;
+`
+export const SpeedStyle = styled.div`
+  border-right: solid 1px #000000;
+  position: relative;
+  width: 15vw;
+`
+export const CountStyle = styled.div`
+  position: relative;
+  width: 15vw;
+`
+export const ChildrenStyle = styled.div`
+  display: flex;
+  flex-direction: row;
+`
