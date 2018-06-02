c071c5d5091c072dca544d876539f897f02e981b
atsuo1203 <dondonkitemasu@gmail.com>
2018-06-02 17:53:08 +0900

    タイトル作成

--- src/components/Top/container/contentsParts/Comment.js
+++ src/components/Top/container/contentsParts/Comment.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import {Div} from './style/CommentStyle'
+import {Div, HeaderDiv,
+  HeaderID, HeaderName, HeaderDate} from './style/CommentStyle'
 
 class Comment extends Component {
   static propTypes = {
   }
 
+  headerParts = () => {
+    return(
+      <HeaderDiv>
+        <HeaderID>{1}</HeaderID>:
+        <HeaderName>たかし</HeaderName>
+        <HeaderDate>2018/06/01(金) 08:24:41.26</HeaderDate>
+      </HeaderDiv>
+    )
+
+  }
+
+  commentParts = () => {
+    return (
+      <div>
+      </div>
+    )
+  }
+
   render() {
     return (
       <Div>
-        hoge
+        {this.headerParts()}
       </Div>
     );
   }
--- src/components/Top/container/contentsParts/style/CommentStyle.js
+++ src/components/Top/container/contentsParts/style/CommentStyle.js
@@ -7,4 +7,27 @@ export const Div = styled.div`
   border-radius: 5px;
   position: relative;
   margin: 5px;
+  background: #F6F6F6;
+`
+
+export const HeaderDiv  = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-left: 3px;
+  margin-right: 3px;
+`
+
+export const HeaderID = styled.span`
+  color: #4f9ff6;
+`
+
+export const HeaderName = styled.span`
+  color: #00ff00;
+  text-shadow: 0.5px 0.5px 0 #000;
+  cursor: pointer;
+  margin-left: 8px;
+  margin-right: 8px;
+`
+export const HeaderDate = styled.span`
+  color: #000000
 `
