f51a61d7e748655ff31d9f823df3814ecb723c36
atsuo1203 <dondonkitemasu@gmail.com>
2018-06-05 18:46:02 +0900

    ルーティング

--- src/index.js
+++ src/index.js
@@ -6,14 +6,24 @@ import Top from './components/Top';
 import reducers from './reducers'
 import {createStore} from 'redux';
 import {Provider} from 'react-redux'
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 // import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(reducers)
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Login />
-  </Provider>,
+  (<Provider store={store}>
+    <MuiThemeProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/login' component={Login} />
+          <Route path='/top' component={Top} />
+        </Switch>
+      </BrowserRouter>
+    </MuiThemeProvider>
+  </Provider>),
   document.getElementById('root')
 );
 // registerServiceWorker();
