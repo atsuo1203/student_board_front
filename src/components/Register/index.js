389f1e0674a6eef9a6445bc1f583952c411b08d0
atsuo1203 <dondonkitemasu@gmail.com>
2018-06-17 17:03:05 +0900

    情報入力

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -9,9 +9,38 @@ import RegisterForm  from './RegisterForm'
 
 class Register extends Component {
   handleLogin = () => {
-    const {email, password} = this.props
-    console.log(email, password)
+    const {password, secondPassword, nickName, twitterName, profile} = this.props
+    console.log('password', password)
+    console.log('secondPassword', secondPassword)
+    console.log('nickName', nickName)
+    console.log('twitterName', twitterName)
+    console.log('profile', profile)
     this.props.history.push('./top')
+  }
+
+  handleChangePassword = (event) => {
+    const {actions} = this.props
+    actions.setPassword(event.target.value)
+  }
+
+  handleChangeSecondPassword = (event) => {
+    const {actions} = this.props
+    actions.setSecondPassword(event.target.value)
+  }
+
+  handleChangeNickName = (event) => {
+    const {actions} = this.props
+    actions.setNickName(event.target.value)
+  }
+
+  handleChangeTwitterName = (event) => {
+    const {actions} = this.props
+    actions.setTwitterName(event.target.value)
+  }
+
+  handleChangeProfile = (event) => {
+    const {actions} = this.props
+    actions.setProfile(event.target.value)
   }
 
   render() {
@@ -20,6 +49,11 @@ class Register extends Component {
         <CommonHeader title='登録ページ'/>
         <RegisterForm
           onClickRegister={this.handleLogin}
+          onChangePassword={this.handleChangePassword}
+          onChangeSecondPassword={this.handleChangeSecondPassword}
+          onChangeNickName={this.handleChangeNickName}
+          onChangeTwitterName={this.handleChangeTwitterName}
+          onChangeProfile={this.handleChangeProfile}
         />
       </div>
     );
@@ -27,6 +61,11 @@ class Register extends Component {
 }
 
 const mapStateToProps = (store) => ({
+  password: store.Register.password,
+  secondPassword: store.Register.secondPassword,
+  nickName: store.Register.nickName,
+  twitterName: store.Register.twitterName,
+  profile: store.Register.profile,
 })
 
 const mapDispatchToProps = (dispatch) => ({
--- src/modules/Register/action.js
+++ src/modules/Register/action.js
@@ -1,10 +1,42 @@
-function setEmail(email){
+function setPassword(password){
   return {
-    type: "SET_EMAIL",
-    payload: email,
+    type: "SET_PASSWORD",
+    payload: password,
+  }
+}
+
+function setSecondPassword(secondPassword){
+  return {
+    type: "SET_SECOND_PASSWORD",
+    payload: secondPassword,
+  }
+}
+
+function setNickName(nickName){
+  return {
+    type: "SET_NICK_NAME",
+    payload: nickName,
+  }
+}
+
+function setTwitterName(twitterName){
+  return {
+    type: "SET_TWITTER_NAME",
+    payload: twitterName,
+  }
+}
+
+function setProfile(profile){
+  return {
+    type: "SET_PROFILE",
+    payload: profile,
   }
 }
 
 export default ({
-  setEmail,
+  setPassword,
+  setSecondPassword,
+  setNickName,
+  setTwitterName,
+  setProfile,
 })
